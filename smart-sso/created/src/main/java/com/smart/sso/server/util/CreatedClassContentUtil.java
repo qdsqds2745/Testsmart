529671a1f200cd28af41cb8ca48e8acd39da6122
Administrator <Administrator@USERCHI-81NMJ10>
2017-12-28 23:23:04 +0800

    xml 弄好了

--- smart-sso/created/src/main/java/com/smart/sso/server/util/CreatedClassContentUtil.java
+++ smart-sso/created/src/main/java/com/smart/sso/server/util/CreatedClassContentUtil.java
@@ -2,6 +2,7 @@ package com.smart.sso.server.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import com.smart.mvc.util.StringUtils;
 import com.smart.sso.server.enums.TypeEnum;
@@ -61,15 +62,80 @@ public class CreatedClassContentUtil {
 	 */
 	public static boolean  CreatedMybatisXmlContent(String xmlPackage,DBTable dbTable,String javaName,String daoPackage){
 		StringBuffer sb = new StringBuffer();
+		List<Column> cs =	dbTable.getColums();
 		//头文件
 		sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\r");
 		sb.append("<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\" >\r");
 		//映射的dao
-		sb.append("<mapper namespace=\"").append(daoPackage.replace(File.separator, ".")).append(javaName).append("Dao\">\r");
+		sb.append("<mapper namespace=\"").append(daoPackage.replace(File.separator, ".")).append(javaName).append("Dao\">\r\r\r");
 		
-		sb.append("<select id=\"query\" parameterType=\"").append(javaName).append("\" resultMap=\"").append(javaName).append("\">\r");//resultMap="databaseMap" parameterType="Database">
-		sb.append("select * from ").append(dbTable.getTableName()).append("\r");
+		sb.append("<select id= \"query\" parameterType=\"").append(javaName).append("\" resultMap=\"").append(javaName).append("\">\r");//resultMap="databaseMap" parameterType="Database">
+		sb.append("     select * from ").append(dbTable.getTableName()).append("\r");
 		sb.append("</select>\r\r");
+		
+		sb.append("<insert id= \"add\" parameterType=\"").append(javaName).append("\" statementType=\"PREPARED\" useGeneratedKeys=\"true\" keyProperty=\"id\">\r");
+		//insert 语句
+		sb.append("    INSERT INTO ").append(dbTable.getTableName()).append("(\r");
+		
+		for (int i = 0; i < cs.size(); i++) {
+			if( i == 0 ){
+				sb.append(cs.get(i).getColumName());
+				
+			}else{
+				sb.append(",").append(cs.get(i).getColumName());
+				
+			}
+		}
+		sb.append(")\r     VALUES (");
+		for (int i = 0; i < cs.size(); i++) {
+			if( i == 0 ){
+				sb.append("#{").append(cs.get(i).getColumName()).append("}");
+				
+			}else{
+				sb.append(",").append("#{").append(cs.get(i).getColumName()).append("}");
+				
+			}
+		}
+		sb.append(")\r");
+		sb.append("</insert>\r\r");
+		
+		//update 语句
+		
+		sb.append("<update id= \"update\" parameterType=\"").append(javaName).append("\" statementType=\"PREPARED\">\r");
+		sb.append("      UPDATE ").append(dbTable.getTableName()).append(" SET \r");
+		for (int i = 1; i < cs.size(); i++) {
+			if( i == 1 ){
+				sb.append(cs.get(i).getColumName()).append(" = #{").append(cs.get(i).getColumName()).append("}");
+				
+			}else{
+				sb.append(",").append(cs.get(i).getColumName()).append(" = #{").append(cs.get(i).getColumName()).append("}");
+				
+			}
+		}
+		sb.append("\r     WHERE id = #{id} \r");
+		sb.append("</update>\r\r");
+		
+		//del 语句
+		
+		sb.append("<delete id=\"deleteById\" parameterType=\"list\" statementType=\"PREPARED\">\r");
+		sb.append("     DELETE FROM ").append(dbTable.getTableName());
+		sb.append("\r    <choose>\r");
+		sb.append("         <when test=\"list == null or list.size() == 0\">\r");
+		sb.append("             WHERE 1 != 1\r");
+		sb.append("         </when>\r");
+		sb.append("         <when test=\"list.size() == 1\">\r");
+		sb.append("             WHERE `id` = <foreach collection=\"list\" item=\"id\">#{id}</foreach>\r");
+		sb.append("         </when>\r");
+		sb.append("         <otherwise>\r");
+		sb.append("            WHERE `id` in <foreach collection=\"list\" item=\"id\" open=\"(\" separator=\",\" close=\")\">#{id}</foreach>\r");
+		sb.append("         </otherwise>\r");
+		sb.append("     </choose>\r");
+		sb.append("</delete>\r\r");
+		
+		
+		
+		
+		
 		sb.append("</mapper>");
 		try {
 			CreatedFileUtil.writeFileContent("D:\\javaCreated\\com\\tcb\\created\\mapper\\SysApp.xml", sb.toString());
